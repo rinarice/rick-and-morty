79ad2debcb09d5d20fb7c3231169062b2a311030
Kateryna <kat.rin47u@gmail.com>
2025-01-16 01:52:37 +0000

    feat: Add Character model and create initial migrations

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8@qzrpfy0jw44#*@!@i2^p-%v9j=9zd6d)w9+e38xd+q&%al&v"
+SECRET_KEY = (
+    "django-insecure-8@qzrpfy0jw44#*@!@i2^p-%v9j=9zd6d)w9+e38xd+q&%al&v"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "characters"
+    "characters",
 ]
 
 MIDDLEWARE = [
@@ -55,8 +57,7 @@ ROOT_URLCONF = "rick_and_morty_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
